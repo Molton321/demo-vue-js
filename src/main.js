6cc13aba05525652e61c3047abace40540bf6a78
felipebuitragocarmona <felipebuitragocarmona@gmail.com>
2025-02-18 11:28:00 -0500

    Interceptores

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import ToastService from 'primevue/toastservice';
 
 import '@/assets/styles.scss';
 import '@/assets/tailwind.css';
+import axios from 'axios';
 import { createPinia } from 'pinia';
 
 const app = createApp(App);
@@ -29,3 +30,28 @@ app.use(ToastService);
 app.use(ConfirmationService);
 
 app.mount('#app');
+
+// Añadir un interceptor global para todas las solicitudes
+axios.interceptors.request.use(
+    (config) => {
+        // Definir rutas que deben estar excluidas del token
+        const excepciones = ['/login', '/public']; // Rutas a excluir
+
+        // Verificar si la URL de la solicitud no está en la lista de excepciones
+        if (!excepciones.includes(config.url)) {
+            // Obtener el token del almacenamiento local (o de donde lo guardes)
+
+            const user = JSON.parse(localStorage.getItem('user'));
+            console.log("agregando token " + user["token"]);
+            if (user) {
+                // Si el token existe, agregarlo al encabezado Authorization
+                config.headers['Authorization'] = `Bearer ${user["token"]}`;
+            }
+        }
+        return config;
+    },
+    (error) => {
+        // Manejar cualquier error en la solicitud
+        return Promise.reject(error);
+    }
+);
